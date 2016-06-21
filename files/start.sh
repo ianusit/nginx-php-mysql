d83bcee2a14dcc71bb0d88cc1209d43f2c9a16f0
amuehlbauer <andreas.muehlbauer@ianus-it.de>
2016-06-21 18:35:13 +0200

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 sed -i "s/SERVERNAME/${SERVERNAME}/g" /etc/nginx/sites-enabled/web
-cp /etc/nginx/sites-enabled/web /etc/nginx/sites-available/web
 php-fpm -D
 nginx -c /etc/nginx/nginx.conf
