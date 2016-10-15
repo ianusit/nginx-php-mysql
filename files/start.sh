0a37d9d9d650f72b90397f2e9585f117a5e1458c
amuehlbauer <andreas.muehlbauer@ianus-it.de>
2016-10-15 09:25:29 +0200

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 sed -i "s/SERVERNAME/${SERVERNAME}/g" /etc/nginx/sites-enabled/web
+sed -i "s/SSLCERTIFICATE/${SSLCERTIFICATE}/g" /etc/nginx/sites-enabled/web
+sed -i "s/SSLCERTIFICATEKEY/${SSLCERTIFICATEKEY}/g" /etc/nginx/sites-enabled/web
 php-fpm -D
 nginx -c /etc/nginx/nginx.conf
